6ea65cf668a86e298892a11211c6de18dc3907d5
William <WiWang@nltechdev.com>
2021-04-27 16:00:24 +0800

    add <PackageReference Include="CompareNETObjects" Version="4.73.0" /> compare pkg

--- TestProjects/Vs2017NetFrameTest/Core31TestProject/Models/TestBaseForMainEntrance.cs
+++ TestProjects/Vs2017NetFrameTest/Core31TestProject/Models/TestBaseForMainEntrance.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KellermanSoftware.CompareNetObjects;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,5 +8,13 @@ namespace Core31TestProject.Models
     public abstract class TestBaseForMainEntrance
     {
         public abstract void MainRun();
+        public static bool CommonCompare<T>(T result, T expectation, ComparisonConfig comparisonConfig = null)
+        {
+            if (result == null && expectation == null) return true;
+
+            var compare = comparisonConfig == null ? new CompareLogic() : new CompareLogic(comparisonConfig);
+            ComparisonResult comparisonResult = compare.Compare(expectation, result);
+            return comparisonResult.AreEqual;
+        }
     }
 }
