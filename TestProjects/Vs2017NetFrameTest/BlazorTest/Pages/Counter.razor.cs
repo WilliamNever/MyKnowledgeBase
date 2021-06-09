b058f6fc6705d478e2d0f63608ec82b8a05e8b32
William <WiWang@nltechdev.com>
2021-06-09 16:16:35 +0800

    add info

---
+++ TestProjects/Vs2017NetFrameTest/BlazorTest/Pages/Counter.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTest.Pages
+{
+    public class CounterBase : Microsoft.AspNetCore.Components.ComponentBase
+    {
+        [Parameter]
+        public int currentCount { get; set; } = 0;
+
+        protected bool isShow = true;
+
+        protected void IncrementCount()
+        {
+            currentCount++;
+        }
+        protected void SOH()
+        {
+            isShow = !isShow;
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+        }
+    }
+}
